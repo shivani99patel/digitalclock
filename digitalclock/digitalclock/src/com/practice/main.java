87d443a25db1b943eb3548cc4d9342670cfb8c90
shivani99patel <69526772+shivani99patel@users.noreply.github.com>
2020-10-20 13:22:50 +0530

    Add files via upload

---
+++ digitalclock/digitalclock/src/com/practice/main.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+
+public class main 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Testing....");
+        MyWindow w=new MyWindow();
+        
+    }
+    
+}
